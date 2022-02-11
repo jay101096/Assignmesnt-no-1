6256eb01e8e4e10a75e1f4d0dbc529195b4f82e0
JAYRAM RAMKRISHNA PRABHU <97171830+jay101096@users.noreply.github.com>
2022-02-11 13:40:22 +0530

    Add files via upload

---
+++ Assignment no.1.py
@@ -0,0 +1,43 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[1]:
+
+
+#Assignment no. 1
+
+
+# In[5]:
+
+
+#Q.1
+for i in range(2000, 3201):
+    if i % 7 == 0 and i % 5 :
+        print(i)
+        str(i).split()
+
+
+# In[12]:
+
+
+#Q.2
+first_name = input("enter your first name: ")
+last_name = input("enter your last name: ")
+print(first_name[::-1], last_name[::-1])
+
+
+# In[18]:
+
+
+#Q.3
+d = int(input("Enter the diameter of sphere in cm: "))
+r = d // 2
+V = (4 / 3) * (22 / 7) * (r ** 3)
+print(V)
+
+
+# In[ ]:
+
+
+
+
